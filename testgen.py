7ccccced87e4ec6ee0b7ae98b5a028fe59abffd1
Tomasz Sapletta <tom@sapletta.com>
2017-01-29 16:30:15 +0100

    files for test generator

--- testgen.py
+++ testgen.py
@@ -8,8 +8,39 @@ def getTestDir():
         if os.path.isdir(dir):
             return dir
 
-list = glob.glob( os.path.join(getTestDir(), "*", "*.php"))
+def getSrcDir():
+    for dir in ['src','app']:
+        if os.path.isdir(dir):
+            return dir
 
-for path in list:
-    name = path.replace(os.path.join(getTestDir(),"") , '')
-    print(name)
+def getSrcList():
+    list = glob.glob( os.path.join(getSrcDir(),"*.php"))
+    for name in list:
+        #name = path.replace(os.path.join(getSrcDir(),"") , '')
+        #name = name.replace(".php" , '')
+        print(name)
+
+def getTestList():
+    list = glob.glob( os.path.join(getTestDir(), "*", "*.php"))
+    for path in list:
+        name = path.replace(os.path.join(getTestDir(),"") , '')
+        #name = name.replace(".php" , '')
+        print(name)
+
+def getCommmand(testIn):
+    testFunction = '1'
+    #testIn = ''
+    testOut = '3'
+    return testFunction + ' ' + testIn + ' ' + testOut
+
+def createFileTests(source):
+    list = glob.glob( os.path.join(source,"*.php"))
+    for name in list:
+        #name = path.replace(os.path.join(source,"") , '')
+        #name = name.replace(".php" , '')
+        test = getCommmand(testIn = name)
+        #print(name)
+        print(test)
+
+print(createFileTests(source=getSrcDir()))
+#getSrcList()
